9990b0fd711d20fb44d042b9349bfddac04d3ca7
PRIGARINA-VICTORIA <99180874+PRIGARINA-VICTORIA@users.noreply.github.com>
2022-06-06 00:19:49 +0300

    Add files via upload

--- include/tree.h
+++ include/tree.h
@@ -1,62 +1,54 @@
-// Copyright 2022 NNTU-CS
+// Copyright 2022 NNTU - CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
-#include <iostream>
 #include <vector>
+#include <iostream>
 #include <string>
+
 class Tree {
  private:
-	struct  Branch {
-		char Data;
-		std::vector<Branch*> subsidiary;
+  struct  Branch {
+    char Data;
     std::vector<Branch*> subsidiary;
-	};
-	Branch* root;
+  };
+  Branch* root;
+  std::vector <char> val;
+  std::string element;
+  std::vector<char> trunsf;
+  std::vector <std::vector<char>> res;
 
-	std::vector <char> val;
-	std::string element;
-	std::vector<char> trunsf;
-	std::vector <std::vector<char>> res;
-
-  public:
-	explicit Tree(std::vector <char> in) {
-   public:
+ public:
   explicit Tree(std::vector <char> in) {
-		val = in;
-		element.resize(in.size());
-		trunsf.resize(in.size());
-		Branch* root = new Branch;
-		root->Data = '*';
-		transformm(in, -1, 0, root);
-	}
-
-	Branch* Create_Tree(char val) {
-		Branch* temp = new Branch;
-		temp->Data = val;
-		return temp;
-	}
-	void transformm(std::vector <char> in, int b, int num, Branch* root) {
-		if (in.size() == 1) {
-			res.push_back(trunsf);
-			return;
-		}
-		auto c = in.cbegin();
-		if (b >= 0) {
+    val = in;
+    element.resize(in.size());
+    trunsf.resize(in.size());
+    Branch* root = new Branch;
+    root->Data = '*';
+    transformm(in, -1, 0, root);
+  }
+  Branch* Create_Tree(char val) {
+    Branch* temp = new Branch;
+    temp->Data = val;
+    return temp;
+  }
+  void transformm(std::vector <char> in, int b, int num, Branch* root) {
+    if (in.size() == 1) {
+      res.push_back(trunsf);
+      return;
+    }
+    auto c = in.cbegin();
     if (b >= 0) {
-			num++;
-			in.erase(c + b);
-		}
-		for (int i = 0; i < in.size(); i++) {
-			root->subsidiary.push_back(Create_Tree(in[i]));
-			trunsf[num] = in[i];
+      num++;
+      in.erase(c + b);
+    }
+    for (int i = 0; i < in.size(); i++) {
+      root->subsidiary.push_back(Create_Tree(in[i]));
       trunsf[num] = in[i];
-			transformm(in, i, num, root->subsidiary[i]);
-		}
-	}
-	std::vector<char> getPerm(Tree tree, int num) {
-		if (num > res.size())return{};
-		return res[num - 1];
-	}
+      transformm(in, i, num, root->subsidiary[i]);
+    }
+  }
+  std::vector<char> getPerm(Tree tree, int num) {
+    if (num > res.size())return{};
+    return res[num - 1];
+  }
 };
-
-#endif  // INCLUDE_TREE_H_
